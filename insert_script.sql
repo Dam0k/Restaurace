aaba0ff6b2f0689e6c335bd4d0e41679d3864016
Dam0k <77647982+Dam0k@users.noreply.github.com>
2023-10-10 21:10:06 +0200

    final version

--- insert_script.sql
+++ insert_script.sql
@@ -4,7 +4,13 @@ show columns from objednavka;
 show columns from rezervace;
 show columns from restaurace;
 
-insert into objednavka (name,price, count) values ('Margherita', '199', '2');
+INSERT INTO objednavka (name, price, count) VALUES ('Margherita', '199Kč', '');
+INSERT INTO objednavka (name, price, count) VALUES ('Quattro Formaggi', '229Kč', '');
+INSERT INTO objednavka (name, price, count) VALUES ('Salame', '189Kč', '');
+INSERT INTO objednavka (name, price, count) VALUES ('Capricciosa', '199Kč', '');
+INSERT INTO objednavka (name, price, count) VALUES ('Hawaii', '199Kč', '');
+INSERT INTO objednavka (name, price, count) VALUES ('Diavola', '209Kč', '');
+INSERT INTO objednavka (name, price, count) VALUES ('Caesar salát', '79Kč', '');
 select * from objednavka;
 
 insert into rezervace (name, email, phone, personcount, datetime, note) values ('damian', 'damian@gmail.com', '+420604612660', '6', '2023-5-1 12:25:53', 'nic');
